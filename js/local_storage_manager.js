10f77fe2a4bb0ece5040146260e7f2581fe44ab3
chenxuan <523764720@qq.com>
2017-11-20 21:39:59 +0800

    禁用2048的localstorage存储游戏状态的功能

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -41,7 +41,7 @@ LocalStorageManager.prototype.localStorageSupported = function () {
 
 // Best score getters/setters
 LocalStorageManager.prototype.getBestScore = function () {
-  return this.storage.getItem(this.bestScoreKey) || 0;
+  return /*this.storage.getItem(this.bestScoreKey) || */0;
 };
 
 LocalStorageManager.prototype.setBestScore = function (score) {
@@ -50,8 +50,8 @@ LocalStorageManager.prototype.setBestScore = function (score) {
 
 // Game state getters/setters and clearing
 LocalStorageManager.prototype.getGameState = function () {
-  var stateJSON = this.storage.getItem(this.gameStateKey);
-  return stateJSON ? JSON.parse(stateJSON) : null;
+  // var stateJSON = this.storage.getItem(this.gameStateKey);
+  return /*stateJSON ? JSON.parse(stateJSON) : */null;
 };
 
 LocalStorageManager.prototype.setGameState = function (gameState) {
