5d0ef163866e47892b70d3586278a75989707114
chenxuan <523764720@qq.com>
2017-11-21 01:35:07 +0800

    为了方便index.js获取游戏控制器对象,搞了个全局的G2048变量

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  G2048 = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 });
