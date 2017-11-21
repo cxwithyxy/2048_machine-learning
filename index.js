65d4565b0204a986bdfa6599163c922430e4b8b1
chenxuan <523764720@qq.com>
2017-11-21 14:58:19 +0800

    修复一个很智障的问题,就是由于settimeout没有处理好,导致在某段时间会拼命杀死和诞生下一代

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var ProjectName = 'Neuroevolution_2048';
 
 var Neuvol = new Neuroevolution({
     population:50,
-    network:[16, [14], 4],
-    nbChild:25
+    network:[16, [14], 4]
 });
+
+var generationCount = 0;
 
 var G = null;
 
@@ -16,6 +17,7 @@ if(localStorage.getItem(ProjectName)){
 }else{
     G = Neuvol.nextGeneration();
 }
+generationCount ++;
 
 /**
  * 初始化死亡列表
@@ -50,6 +52,114 @@ function isAllDead()
     return allDead;
 }
 
+var frameLoopHL = 0;
+function startAIFrame()
+{
+    frameLoopHL = setInterval(function ()
+    {
+        eachIframe(function (_win, _index)
+        {
+            if(!G_deaded[_index]){
+                var theCells = _win.G2048.grid.cells;
+                
+                var theInput = [];
+                for(var i = 0; i < 4; i++){
+                    for(var j = 0; j < 4; j++){
+                        var pushInArray = 0;
+                        if(theCells[i][j]){
+                            pushInArray = theCells[i][j].value;
+                        }
+                        theInput.push(pushInArray);
+                    }
+                }
+
+                // var res = G[_index].compute(theInput)[0];
+
+                // res *= 4;
+                // if(res >=0 && res <1 ){
+                //     _win.G2048.move(0);
+                // }
+                // if(res >=1 && res <2 ){
+                //     _win.G2048.move(1);
+                // }
+                // if(res >=2 && res <3 ){
+                //     _win.G2048.move(2);
+                // }
+                // if(res >=3 && res <4 ){
+                //     _win.G2048.move(3);
+                // }
+
+                var res = G[_index].compute(theInput);
+                var resObjList = [];
+                for(var i = 0; i < res.length; i++){
+                    resObjList.push({index: i, value: res[i]});
+                }
+                resObjList.sort(function (a, b)
+                {
+                    if(a.value > b.value){
+                        return false;
+                    }
+                    return true;
+                })
+                _win.G2048.move(resObjList[0].index);
+                
+
+                if(lastInputString[_index] == JSON.stringify(theInput)){
+                    // G_deaded[_index] = 1;
+                    G_deaded[_index] = 2;
+                }
+
+                if(_win.G2048.serialize().over){
+                    G_deaded[_index] = 2;
+                }
+
+                lastInputString[_index] = JSON.stringify(theInput);
+
+            }
+        });
+        if(isAllDead()){
+            clearInterval(frameLoopHL);
+            /**
+             * 
+             */
+            setTimeout(function ()
+            {
+                eachIframe(function (_win, _index){
+                    // var theCells = _win.G2048.grid.cells;
+                    // var theInput = [];
+                    // for(var i = 0; i < 4; i++){
+                    //     for(var j = 0; j < 4; j++){
+                    //         var pushInArray = 0;
+                    //         if(theCells[i][j]){
+                    //             pushInArray = theCells[i][j].value;
+                    //         }
+                    //         theInput.push(pushInArray);
+                    //     }
+                    // }
+                    // theInput.sort();
+                    // Neuvol.networkScore(G[_index], theInput.pop());
+                    Neuvol.networkScore(G[_index], _win.G2048.serialize().score);
+                    if(G_deaded[_index] == 2){
+                        _win.G2048.restart();
+                    }
+                });
+                G = Neuvol.nextGeneration();
+
+                // var savingData = [];
+                // for(var i = 0; i < G.length; i++){
+                //     savingData.push(G[i].getSave());
+                // }
+                // localStorage.setItem(ProjectName, JSON.stringify(savingData));
+                
+                G_deaded = [];
+                lastInputString = [];
+                generationCount ++;
+                console.log("第" + generationCount + "代");
+                startAIFrame();
+            },300);
+        }
+    },100)
+}
 
 setTimeout(function (){
     var body = document.getElementsByTagName('body')[0];
@@ -62,84 +172,6 @@ setTimeout(function (){
     }
     setTimeout(function ()
     {
-        setInterval(function ()
-        {
-            eachIframe(function (_win, _index)
-            {
-                if(!G_deaded[_index]){
-                    var theCells = _win.G2048.grid.cells;
-                    
-                    var theInput = [];
-                    for(var i = 0; i < 4; i++){
-                        for(var j = 0; j < 4; j++){
-                            var pushInArray = 0;
-                            if(theCells[i][j]){
-                                pushInArray = theCells[i][j].value;
-                            }
-                            theInput.push(pushInArray);
-                        }
-                    }
-
-                    var res = G[_index].compute(theInput);
-
-                    var resObjList = [];
-                    for(var i = 0; i < res.length; i++){
-                        resObjList.push({index: i, value: res[i]});
-                    }
-                    resObjList.sort(function (a, b)
-                    {
-                        if(a.value > b.value){
-                            return false;
-                        }
-                        return true;
-                    })
-                    _win.G2048.move(resObjList[0].index);
-                    
-
-                    if(lastInputString[_index] == JSON.stringify(theInput)){
-                        // G_deaded[_index] = 1;
-                        G_deaded[_index] = 2;
-                    }
-
-                    if(_win.G2048.serialize().over){
-                        G_deaded[_index] = 2;
-                    }
-
-                    lastInputString[_index] = JSON.stringify(theInput);
-
-                }
-            });
-            if(isAllDead()){
-
-                /**
-                 * 
-                 */
-                setTimeout(function ()
-                {
-                    eachIframe(function (_win, _index){
-                        var theCells = _win.G2048.grid.cells;
-                        var theInput = [];
-                        for(var i = 0; i < 4; i++){
-                            for(var j = 0; j < 4; j++){
-                                var pushInArray = 0;
-                                if(theCells[i][j]){
-                                    pushInArray = theCells[i][j].value;
-                                }
-                                theInput.push(pushInArray);
-                            }
-                        }
-                        theInput.sort();
-                        Neuvol.networkScore(G[_index], theInput.pop());
-                        // Neuvol.networkScore(G[_index], _win.G2048.serialize().score);
-                        if(G_deaded[_index] == 2){
-                            _win.G2048.restart();
-                        }
-                    });
-                    G = Neuvol.nextGeneration();
-                    G_deaded = [];
-                    lastInputString = [];
-                },1000);
-            }
-        },100)
+        startAIFrame();
     }, 10000)
 });
